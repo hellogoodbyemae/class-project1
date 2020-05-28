ac355160fea04bc75334a7440a088faa2a3fe08f
Nick <nicholasroman86@gmail.com>
2020-05-27 21:04:42 -0400

    Fixed opacity on background

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,15 +9,74 @@ var queryJob = "";
 var jobLocation = "";
 
 //queryURL
-var queryURL = "https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=" + adzId + "&app_key=" + adzKey;
+var queryURLBase = "https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=" + adzId + "&app_key=" + adzKey;
 
 // FUNCTIONS
-function runQuery(response, queryURL){
-    $.ajax({url: queryURL, method: "GET", Accept: "application/json"})
-        .done(function(jobData) {
-            console.log(queryURL);
+function runQuery(response, queryURLBase){
+    $.ajax({url: queryURLBase, method: "GET", Accept: "application/json"})
+        .then(function(jobData) {
+            console.log(queryURLBase);
             console.log(jobData);
-        })
+
+            for (var i=0; i<jobData.results.length; i++){
+                console.log(jobData.results[i].title);
+                console.log(jobData.results[i].description);
+                console.log(jobData.results[i].company.display_name);
+                console.log(jobData.results[i].location.display_name);
+                console.log(jobData.results[i].redirect_url);
+
+                // $("#results-view").text(JSON.stringify(jobData));
+                jobDiv = $("#results-view");
+                // jobDiv.append(class; job)
+
+                var title = jobData.results[i].title;
+                var pOne = $("<h4>").text(title);
+                jobDiv.append(pOne);
+
+                var description = jobData.results[i].description;
+                var pTwo = $("<p>").text(description);
+                jobDiv.append(pTwo);
+
+                var dispName = jobData.results[i].company.display_name;
+                var pThree = $("<p>").text(dispName);
+                jobDiv.append(pThree);
+
+                var location = jobData.results[i].location.display_name;
+                var pFour = $("<p>").text(location);
+                jobDiv.append(pFour);
+
+                var url = jobData.results[i].redirect_url;
+                var pFive = $("<a>").text(url);
+                jobDiv.append("<a href='" + url + "'>" + url + "</a>");
+
+                $("#results-view").prepend(jobDiv);
+
+            }
+
+            
+        });
 }
-runQuery("", queryURL);
+
+function displayResults(){
+
+}
+
 // PROCESS
+$("#search").on("click", function(event) {
+
+    event.preventDefault();
+
+    queryJob = $("#job-search").val().trim();
+    console.log(queryJob);
+
+    jobLocation = $("#location-search").val().trim();
+    console.log(jobLocation);
+
+    //Add in Search Term
+    var newURL = queryURLBase + "&what=" + queryJob + "&where=" + jobLocation + "&content-type=application/json";
+    console.log(newURL);
+
+    runQuery(10, newURL);
+
+})
+
